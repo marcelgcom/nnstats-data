11b133c306efc3be9138d4ccf30f3db579a78a11
Marcel <marcel.gheorghita@outlook.com>
2021-10-13 15:22:48 +0200

    Add latest event info

--- copyData.bat
+++ copyData.bat
@@ -1,5 +1,6 @@
 copy /Y ..\stt\output\*crew.json
 copy /Y ..\stt\output\tierConfig.json
+copy /Y ..\stt\output\events.json
 copy /Y ..\stt\gauntlets\*.json gauntlets\.
 git add .
 git commit -m "%*"
