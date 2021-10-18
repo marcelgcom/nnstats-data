9dc547bc4efc92658e3fd0c45886576dcff88420
Marcel <marcel.gheorghita@outlook.com>
2021-10-18 19:07:49 +0200

    Add event 284 leaderboard and sync BB

--- copyData.bat
+++ copyData.bat
@@ -2,6 +2,7 @@ copy /Y ..\stt\output\*crew.json
 copy /Y ..\stt\output\tierConfig.json
 copy /Y ..\stt\output\events.json
 copy /Y ..\stt\gauntlets\*.json gauntlets\.
+git status
 git add .
 git commit -m "%*"
 git push
