3c1887ca086c0627b4e22d17245c603c68be4a0f
Marcel <marcel.gheorghita@outlook.com>
2021-12-28 18:01:09 +0100

    Add Marlena Moreau

--- copyData.bat
+++ copyData.bat
@@ -1,7 +1,5 @@
-copy /Y ..\stt\output\*crew.json
 copy /Y ..\stt\output\tierConfig.json
 copy /Y ..\stt\output\events.json
-copy /Y ..\stt\gauntlets\*.json gauntlets\.
 git status
 git add .
 git commit -m "%*"
