ec6c9c4b31b89f085146fc83df6a6e986f1ee172
Marcel <marcel.gheorghita@outlook.com>
2022-04-07 21:02:02 -0700

    Update Rumplestiltskin art

--- copyData.bat
+++ copyData.bat
@@ -1,7 +1,7 @@
-copy /Y ..\stt\data\tierConfig.json
-copy /Y ..\stt\data\events.json
-copy /Y ..\stt\output\crew.json
-copy /Y ..\stt\gauntlets\*.json gauntlets\.
+copy /Y ..\bigbook\data\tierConfig.json
+copy /Y ..\bigbook\data\events.json
+copy /Y ..\bigbook\output\crew.json
+copy /Y ..\bigbook\gauntlets\*.json gauntlets\.
 git status
 git add .
 git commit -m "%*"
